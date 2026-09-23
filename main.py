3198d24238075ec684bf949e823c2567517b91a1
nguyenhaley <130112171+nguyenhaley@users.noreply.github.com>
2026-09-23 15:03:49 -0700

    added netlify site to accept logins/signups

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174"],
+    allow_origins=["http://localhost:5173", "http://localhost:5174", "https://wondrous-syrniki-efd098.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
